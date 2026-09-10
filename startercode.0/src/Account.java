232b8db2f3155f03be30b6646d6d0b0fe8a23d45
Hanru Sun <hanks@andrew.cmu.edu>
2026-09-09 21:21:15 -0700

    new files added

---
+++ startercode.0/src/Account.java
@@ -0,0 +1,83 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Account  {
+	
+	// the unique user name of account owner
+	private String userName;
+	
+	// list of members who are awaiting an acceptance response from this account's owner 
+	private Set<String> incomingRequests = new HashSet<String>();
+	private Set<String> outgoingRequests = new HashSet<String>();
+	private boolean autoAcceptFriendships = false;
+	
+	// list of members who are friends of this account's owner
+	private Set<String> friends = new HashSet<String>();
+	
+	public Account(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	// return list of members who had sent a friend request to this account's owner 
+	// and are still waiting for a response
+	public Set<String> getIncomingRequests() {
+		return incomingRequests; 
+	}
+
+	public Set<String> getOutgoingRequests() {
+		return outgoingRequests;
+	}
+
+	// an incoming friend request to this account's owner from another member account
+	public void requestFriendship(Account fromAccount) {
+		if (fromAccount == null) return;
+		if (!friends.contains(fromAccount.getUserName())) {
+			incomingRequests.add(fromAccount.getUserName());
+			fromAccount.outgoingRequests.add(this.getUserName());
+			if (this.autoAcceptFriendships) {
+				fromAccount.friendshipAccepted(this);
+			}
+		}
+}
+
+	// check if account owner has a member with user name userName as a friend
+	public boolean hasFriend(String userName) {
+		return friends.contains(userName);
+	}
+
+	// receive an acceptance from a member to whom a friend request has been sent and from whom no response has been received
+	public void friendshipAccepted(Account toAccount) {
+		if (!toAccount.incomingRequests.contains(this.getUserName())) {
+			return;
+		}
+		friends.add(toAccount.getUserName());
+		toAccount.friends.add(this.getUserName());
+		toAccount.incomingRequests.remove(this.getUserName());
+		this.outgoingRequests.remove(toAccount.getUserName());
+	}
+	
+	public Set<String> getFriends() {
+		return friends;
+	}
+
+	public void friendshipRejected(Account myAccount) {
+		myAccount.incomingRequests.remove(this.getUserName());
+    	this.outgoingRequests.remove(myAccount.getUserName());
+	}
+
+	public void autoAcceptFriendships() {
+		autoAcceptFriendships = true;
+	}
+
+	public void cancelFriendship(Account myAccount) {
+		friends.remove(myAccount.getUserName());
+		myAccount.friends.remove(this.getUserName());
+	}
+	
+
+}
---
+++ startercode.0/src/SocialNetwork.java
@@ -0,0 +1,102 @@
+import java.util.HashSet;
+import java.util.Collection;
+
+public class SocialNetwork {
+	
+	private Collection<Account> accounts = new HashSet<Account>();
+
+	// join SN with a new user name
+	public Account join(String userName) {
+		if (userName == null || userName.isEmpty()) return null;
+		if (findAccountForUserName(userName) != null) return null;
+		Account newAccount = new Account(userName);
+		accounts.add(newAccount);
+		return newAccount;
+	}
+
+	// find a member by user name 
+	private Account findAccountForUserName(String userName) {
+		// find account with user name userName
+		// not accessible to outside because that would give a user full access to another member's account
+		for (Account each : accounts) {
+			if (each.getUserName().equals(userName)) 
+					return each;
+		}
+		return null;
+	}
+	
+	// list user names of all members
+	public Collection<String> listMembers() {
+		Collection<String> members = new HashSet<String>();
+		for (Account each : accounts) {
+			members.add(each.getUserName());
+		}
+		return members;
+	}
+	
+	// from my account, send a friend request to user with userName from my account
+	public void sendFriendshipTo(String userName, Account me) {
+		try {
+			Account accountForUserName = findAccountForUserName(userName);
+			accountForUserName.requestFriendship(me);
+		} catch (NullPointerException e) {
+			System.out.println("No such user: " + userName);
+		}
+	}
+
+	// from my account, accept a pending friend request from another user with userName
+	public void acceptFriendshipFrom(String userName, Account me) {
+		try {
+			Account accountForUserName = findAccountForUserName(userName);
+			accountForUserName.friendshipAccepted(me);
+		} catch (NullPointerException e) {
+			System.out.println("No such user: " + userName);
+		}
+	}
+
+	public void acceptAllFriendshipsTo(Account me) {
+		Collection<String> requests = new HashSet<>(me.getIncomingRequests());
+		requests.forEach(each -> {
+			Account accountForUserName = findAccountForUserName(each);
+			accountForUserName.friendshipAccepted(me);
+		});
+		me.getIncomingRequests().clear();
+	}
+
+	public void rejectFriendshipFrom(String userName, Account me) {
+		if (me == null || userName == null) return;
+		Account accountForUserName = findAccountForUserName(userName);
+		if (accountForUserName != null) {
+			accountForUserName.friendshipRejected(me);
+		}
+		me.getIncomingRequests().remove(userName);
+	}
+
+	public void rejectAllFriendshipsTo(Account me) {
+		Collection<String> requests = new HashSet<>(me.getIncomingRequests());
+    	requests.forEach(each -> 
+			rejectFriendshipFrom(each, me));
+		me.getIncomingRequests().clear();
+	}
+
+	public void sendFriendshipCancellationTo(String userName, Account me) {
+		Account accountForUserName = findAccountForUserName(userName);
+		accountForUserName.cancelFriendship(me);
+	}
+
+	public void leave(Account me) {
+    for (String each : new HashSet<>(me.getFriends())) {
+        findAccountForUserName(each).cancelFriendship(me);
+    }
+    for (Account each : accounts) {
+        each.getIncomingRequests().remove(me.getUserName());
+        each.getOutgoingRequests().remove(me.getUserName());
+    }
+    accounts.remove(me);
+}
+
+	public void autoAcceptFriendshipsTo(Account me) {
+		me.autoAcceptFriendships();
+	}
+
+}
